931f34fb7f08766411e7c3118a8cee59d7ed5b11
Bruce Chen <bruce.chen@akmii.com>
2018-03-21 19:46:35 +0800

    Init

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Application extends React.Component {
+class CustomButton extends React.Component {
     onClick() {
-        window.alert("Bruce CHen");
+        window.alert("Bruce Chen");
     }
 
     render() {
-        return (<div onClick={this.onClick.bind(this)}>
-            <h1>Hello, this is dynamic script render.</h1>
-        </div>);
+        return <div onClick={this.onClick.bind(this)}>click here</div>;
     }
 }
-ReactDOM.render(<Application />, document.getElementById("app_content"));
+
+class Application extends React.Component {
+    onClick() {
+        window.alert("Bruce Chen");
+    }
+
+    render() {
+        return <div onClick={this.onClick.bind(this)}>
+            <h1>Hello, this is dynamic script render.</h1>
+            <CustomButton></CustomButton>
+        </div>;
+    }
+}
+
+ReactDOM.render(<Application/>, document.getElementById("app_content"));
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,15 @@ const path = require('path');
 
 module.exports = {
     entry: './src/app.jsx',
+    devtool: "source-map",
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: "/"//path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        headers: {"Access-Control-Allow-Origin": "*"},
+        host: '0.0.0.0',
+        port: 8686,
     },
     module: {
         rules: [
