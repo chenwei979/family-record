aa092eb85c71ae79717d872a12d5e76bed7ded47
Bruce Chen <bruce.chen@akmii.com>
2018-03-22 11:20:58 +0800

    Init

---
+++ webpack.dll.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const DLL_PATH = path.resolve(__dirname, 'dll');
+module.exports = {
+    entry: {
+        vendor: ['react', 'react-dom', 'jquery']
+    },
+    output: {
+        path: DLL_PATH,
+        filename: '[name].dll.js',
+        library: '[name]_library'
+    },
+    plugins: [
+        new webpack.DllPlugin({
+            path: path.resolve('./dll', '[name]-manifest.json'),
+            name: '[name]_library'
+        })
+    ]
+};
