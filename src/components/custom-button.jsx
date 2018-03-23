ccd27792e00a2031d138d661d128657da1350df3
Bruce Chen <bruce.chen@akmii.com>
2018-03-23 11:50:40 +0800

    Init

--- src/components/custom-button.jsx
+++ src/components/custom-button.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import $ from 'jquery';
-import {permissonHOC} from "../util";
+import {permissionHOC} from "../util";
 
-@permissonHOC(5, 4)
+@permissionHOC(5, 4)
 export default class CustomButton extends React.Component {
     onClick() {
-        $.post("/api/compile", {
-            id: "chenwei_979",
-            script: `window.alert("Bruce Chen")`,
-            contentType: "js",
-            syntax: "babel",
-            version: "default",
+        fetch("/api/compile", {
+            method: 'POST',
+            body: JSON.stringify({
+                id: "chenwei_979",
+                script: `window.alert("Bruce Chen")`,
+                contentType: "js",
+                syntax: "babel",
+                version: "default",
+            })
+        }).then(response => {
+            return response.json();
+        }).then(data => {
+            console.log(data);
         });
-        window.alert("Bruce Chen");
     }
 
     render() {
